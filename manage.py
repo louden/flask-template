ea4fd0a866eaca8a1390b3aefc6c805f506182e7
Christopher Louden <loudenconsulting@gmail.com>
2016-06-21 14:35:56 -0500

    Add seeddb to manage.py

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 
 import os
 
+from datetime import datetime
 from flask.ext.script import Manager, Server
 from flask.ext.script.commands import ShowUrls, Clean
 from appname import create_app
 from appname.database import db
-from appname.main.models import User
+from appname.main.models import User, Role
 
 # default to dev config because no one should use this in
 # production anyway
@@ -35,6 +36,53 @@ def createdb():
     """
 
     db.create_all()
+
+
+@manager.command
+def seeddb():
+    """ Seeds the database with a user with the user role and one with the
+        admin role
+    """
+
+    # Create all tables
+    db.create_all()
+
+    # Adding roles
+    admin_role = find_or_create_role('admin', u'Admin')
+
+    # Add users
+    find_or_create_user(u'Admin', u'Example', u'admin@example.com',
+                        'Password1', admin_role)
+    find_or_create_user(u'User', u'Example', u'user@example.com',
+                        'Password1')
+
+    # Save to DB
+    db.session.commit()
+
+
+def find_or_create_role(name, label):
+    """ Find existing role or create new role; used by seeddb """
+    role = Role.query.filter(Role.name == name).first()
+    if not role:
+        role = Role(name=name, label=label)
+        db.session.add(role)
+    return role
+
+
+def find_or_create_user(first_name, last_name, email, password, role=None):
+    """ Find existing user or create new user; used by seeddb """
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        user = User(email=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                    password=app.user_manager.hash_password(password),
+                    active=True,
+                    confirmed_at=datetime.utcnow())
+        if role:
+            user.roles.append(role)
+        db.session.add(user)
+    return user
 
 if __name__ == "__main__":
     manager.run()
